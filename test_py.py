7daf1a2457af3aef8fdf13f1b3c3ac134769bc59
GeryHU <109093841+GeryHU@users.noreply.github.com>
2023-01-27 18:24:32 +0100

    code

--- test_py.py
+++ test_py.py
@@ -1,2 +1,13 @@
 #hello my name is testpy 
 #im have one very good code this is my work ;D
+from random import randint
+
+karakterek:str = 'aAe&EoOiIuUdDfFgG#hHjJkKlLp.PzZtTrRqQwWyYxX_cCvVbBnNmM0123456789-'
+jelszo:str= ''
+for n in range(32):
+    jelszo+=karakterek[randint(0,len(karakterek)-1)]
+    
+
+
+
+print(f'Generált jelszó: {jelszo}')
